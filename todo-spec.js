396394feeefea487fc68f4bcaf4361b54f5744af
cacdlatam <cacdlatam@gmail.com>
2017-07-30 19:54:09 -0500

    Update todo-spec.js

--- todo-spec.js
+++ todo-spec.js
@@ -1,7 +1,7 @@
 describe('CDBU Modern Software Factory UI Test - QA Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://pre-raffle-latam.us-east-2.elasticbeanstalk.com');
+    browser.get('http://la-preprod.cdbu.io');
     element(by.css('[name="name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('I\'m In!')).click();
     });
